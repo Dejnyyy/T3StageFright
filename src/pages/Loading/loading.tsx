8c9767b0f2100a94d9ab09e1e60e3fccecf9c0d2
Dejnyyy <danslaby@email.cz>
2024-11-21 09:07:56 -0500

    lepsi

--- src/pages/Loading/loading.tsx
+++ src/pages/Loading/loading.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import styles from "./loading.module.css"; // Import your CSS module
+import styles from "./loading.module.css";
 
 const Loading = () => {
   return (
-    <div className={styles["sound-loader"]}>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
-      <div className={styles.bar}></div>
+    <div className="flex flex-col items-center justify-center h-screen bg-black">
+      <div className={styles["sound-loader"]}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+      <p className="text-white mt-4 text-xl">Loading Stage Fright...</p>
     </div>
   );
 };
